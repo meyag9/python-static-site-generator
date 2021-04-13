03f332ed245275781802e134288ed74397cd7f0f
Meya Gorbea <meya.gorbea@ALWAYSRECENTDRY.workdayinternal.com>
2021-04-13 09:36:14 -0700

    fixing errors

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -30,6 +30,6 @@ class Parser:
 class ResourceParser(Parser):
     extensions = [".jpg", ".png", ".gif", ".css", ".html"]
 
-    def parse(self, path: Path, source: Path, dest: Path):
-    copy(path, source, dest)
+    def parse(self, path, source, dest):
+        self.copy(path, source, dest)
 
